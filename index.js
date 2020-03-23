d26dd6ea925cf8f8e22103183bb4dc7b16afc029
Aqsa48 <aksaumar35@gmail.com>
2020-03-24 00:25:35 +0500

    Add add-Aqsa to the list of contributors

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const app = express();
-const path=require('path');
+const path = require('path');
+
 //Serves resources from public folder
-app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 const fs = require('fs');
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     let rawdata = fs.readFileSync('./Data/member.json');
-let contributors = JSON.parse(rawdata);
-console.log(contributors);
+    let contributors = JSON.parse(rawdata);
+    console.log(contributors);
 
-    res.render('index',{members:contributors})
+    res.render('index', { members: contributors })
 })
+
 //connection to server
+
 app.listen(process.env.PORT, error => {
     if (error) console.log("error is " + error);
     else console.log("listening to port " + 3000);
 
 });
+
+// For running this project locally.
+// app.listen(3000);
